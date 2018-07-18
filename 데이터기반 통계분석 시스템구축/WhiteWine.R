fcefcbf6407032b4ff2440ecb368cd1ea31592f9
BenHeo <32593827+BenHeo@users.noreply.github.com>
2018-07-18 19:19:20 +0900

    endof0718

--- 데이터기반 통계분석 시스템구축/WhiteWine.R
+++ 데이터기반 통계분석 시스템구축/WhiteWine.R
@@ -10,11 +10,31 @@ head(white)
 summary(white)
 str(white)
 attach(white)
+# draw histograms
+hist(fixed_acidity, breaks = 20) # normal with outliers
+hist(volatile_acidity, breaks = 20) # right skew
+hist(citric_acid, breaks = 30) # # outliers 
+hist(residual_sugar, breaks = 10) # right skew
+hist(chlorides, breaks = 20) # some ranges have large number of observations ==> better to log
+hist(log(chlorides), breaks = 20)
+hist(free_sulfur_dioxide, breaks = 30) # almost normal
+hist(total_sulfur_dioxide, breaks = 20) # normal
+hist(density, breaks = 20) # normal but outliers
+hist(pH, breaks = 20) # normal
+hist(sulphates, breaks = 20) # normal
+hist(alcohol, breaks = 30) # almost normal
+hist(quality, breaks = 20) # discrete data, unbalance exists between data
+table(quality) # 20  163  1457  2198  880  175  5
+
+
+# draw boxplots about quality
 boxplot(fixed_acidity~quality, col = 3:9)
 boxplot(volatile_acidity~quality, col = 3:9)
 boxplot(citric_acid~quality, col = 3:9)
 boxplot(residual_sugar~quality, col = 3:9)
 boxplot(chlorides~quality, col = 3:9)
+boxplot(log(chlorides)~quality, col = 3:9)
+
 boxplot(free_sulfur_dioxide~quality, col = 3:9)
 boxplot(total_sulfur_dioxide~quality, col = 3:9)
 boxplot(density~quality, col = 3:9)
