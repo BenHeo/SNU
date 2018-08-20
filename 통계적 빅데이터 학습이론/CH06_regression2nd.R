b2ed242a870eb8c4874d4b876da49724cf4ab8bd
BenHeo <32593827+BenHeo@users.noreply.github.com>
2018-08-20 20:32:00 +0900

    endof0820

--- 통계적 빅데이터 학습이론/CH06_regression2nd.R
+++ 통계적 빅데이터 학습이론/CH06_regression2nd.R
@@ -18,3 +18,47 @@ argmax_adjr2 <- which.max(reg.summary$adjr2)
 points(argmax_adjr2, reg.summary$adjr2[argmax_adjr2], col = "red", pch = 12, cex = 3)
 plot(reg.summary$cp)
 plot(reg.summary$bic)
+
+
+
+
+
+
+
+
+# Lasso Bootstrap
+
+library(glmnet)
+Hitters <- na.omit(Hitters)
+dim(Hitters)
+
+attach(Hitters)
+x = model.matrix(Salary~., Hitters)
+y = Hitters$Salary
+n = 263
+B = 1000
+best = matrix(0, B, 21) # 원래 20개로 했는데 Beta0 이 있어서 1개 추가해줌
+
+for (b in (1:B)){
+  bid = sample(n,n, replace = TRUE)
+  bx = x[bid,]
+  by = y[bid]
+  grid = 10^seq(4, -1, length.out = 100)
+  cv.out = cv.glmnet(bx, by, alpha = 1, lambda = grid) # cross validation 결과
+  blamb = cv.out$lambda.min # 결과가 가장 작은 걸 선택
+  lasso.mod = glmnet(bx,by, alpha = 1, lambda = exp(blamb))
+  best[b,] = as.vector(coef(lasso.mod))
+  cat("\t b=")
+  cat(b)
+}
+best
+mu <- apply(best, 2, mean)
+se <- apply(best, 2, sd)
+ 
+tstat <- mu/se
+pvalue <- 2*(1-pnorm(abs(tstat)))
+pvalue
+select <- (best != 0)
+stab <- apply(select, 2, sum)/B
+numselect <- apply(select, 1, sum)
+hist(numselect, breaks = 20)
